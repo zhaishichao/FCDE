a4b60edb72dde86dc36f3d4861eca76c9d638e89
zsc <2827636950@qq.com>
2025-12-08 21:45:01 +0800

    添加数据集的名称

--- config/parameter_config.py
+++ config/parameter_config.py
@@ -1,4 +1,3 @@
-
 class EvolutionaryParameterConfig:
     def __init__(self, POPSIZE=30, CXPB=0.9, MUTPB=0.1, NGEN=50, verbose=True):
         self.POPSIZE = POPSIZE  # 种群大小
@@ -6,3 +5,8 @@ class EvolutionaryParameterConfig:
         self.MUTPB = MUTPB  # 变异概率
         self.NGEN = NGEN  # 迭代次数
         self.verbose = verbose  # 是否打印信息
+
+
+datasetnames = ['iris0', 'ecoli1', 'glass0', 'glass1', 'haberman', 'pima', 'segment0', 'vowel0', 'wisconsin', 'yeast1',
+                'shuttle-c0-vs-c4']
+datasetnames = ['australian', 'heart', 'phoneme', 'ring', 'spambase', 'wdbc', 'ionosphere']
