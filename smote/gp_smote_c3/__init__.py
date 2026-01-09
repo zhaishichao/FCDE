1a4ed6bcafd058d52b834e74c6c1dc8cd67fa742
zsc <2827636950@qq.com>
2026-01-09 21:01:18 +0800

    精简了原版dssmote（三个约束、两个目标）

---
+++ smote/gp_smote_c3/__init__.py
@@ -0,0 +1,6 @@
+from .ds_smote import DSSMOTE
+# 三个约束，两个目标 原始版本的GSSMOTE (不包含g1)
+# max_g1 = max(avg_distance - ind.distance_minority_min for ind in individuals)  # distance_minority_min > 0
+# max_g2 = max(0 - ind.fitness.values[0] for ind in individuals)  # ind.fitness.values[0] (第一个目标) > 0
+# max_g3 = max(ind.distance_minority_center for ind in individuals)  # distance_minority_center - ave_max_distance > 0
+# max_g4 = max((ind.cosine_angle - 90) for ind in individuals)  # cosine_angle < 90
